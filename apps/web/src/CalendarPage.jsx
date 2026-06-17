797344e05755e0f022771352ff2a088b9cc7a165
Omar Carrillo <ocarrillo@accessq.com.mx>
2026-06-17 14:35:03 -0600

    Revert "Hide vendor selector for non-admin calendar users"
    This reverts commit 27a92a3689f4d93957e7b52c6d0d70efd4d73507.

--- apps/web/src/CalendarPage.jsx
+++ apps/web/src/CalendarPage.jsx
@@ -766,22 +766,21 @@ export default function CalendarPage({ currentUser }) {
               onChange={(event) => setCalendarDate(event.target.value)}
             />
           </label>
-          {canReadAll ? (
-            <label>
-              Vendedor
-              <select
-                value={selectedSellerId}
-                onChange={(event) => setSelectedSellerId(event.target.value)}
-              >
-                <option value="all">Todos</option>
-                {sellers.map((seller) => (
-                  <option key={seller.id} value={seller.id}>
-                    {seller.fullName}
-                  </option>
-                ))}
-              </select>
-            </label>
-          ) : null}
+          <label>
+            Vendedor
+            <select
+              value={selectedSellerId}
+              onChange={(event) => setSelectedSellerId(event.target.value)}
+              disabled={!canReadAll}
+            >
+              {canReadAll ? <option value="all">Todos</option> : null}
+              {sellers.map((seller) => (
+                <option key={seller.id} value={seller.id}>
+                  {seller.fullName}
+                </option>
+              ))}
+            </select>
+          </label>
           <label>
             SLA (dias)
             <input
